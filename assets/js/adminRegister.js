baeacab2728e47f9cf119e7e38b6c5a1d2fe7fbb
KasunUmesh <kasunumesh38@gmail.com>
2023-10-26 02:48:57 +0530

    Save Admin Data send backend function created

--- assets/js/adminRegister.js
+++ assets/js/adminRegister.js
@@ -1,12 +1,9 @@
+// Image Upload and view
+
 const imgDiv = document.querySelector(".profileIMG-wrapper");
 const img = document.querySelector("#photo");
-const file = document.querySelector("#file");
+const file = document.querySelector("#adminIMG");
 const uploadbtn = document.querySelector("#uploadbtn");
-
-console.log(imgDiv);
-console.log(img);
-console.log(file);
-console.log(uploadbtn);
 
 file.addEventListener("change", function () {
   const chosedfile = this.files[0];
@@ -19,3 +16,42 @@ file.addEventListener("change", function () {
     reader.readAsDataURL(chosedfile);
   }
 });
+
+// Frontend and backend functions
+
+var baseUrl = "http://localhost:8083/travelPlanning/admin";
+
+$("#saveAdminBtn").click(function () {
+  saveAdmin();
+});
+
+function saveAdmin() {
+  const formData = new FormData();
+
+  const name = $("#adminName").val();
+  const email = $("#adminEmail").val();
+  const password = $("#adminPassword").val();
+  const image = $("#adminIMG")[0].files[0];
+
+  formData.append("adminName", name);
+  formData.append("adminEmail", email);
+  formData.append("adminPassword", password);
+  formData.append("adminIMG", image);
+
+  $.ajax({
+    url: baseUrl,
+    processData: false,
+    contentType: false,
+    cache: false,
+    method: "POST",
+    data: formData,
+    success: function (res) {
+      if (res.code == 200) {
+        alert(res.message);
+      }
+    },
+    error: function (res) {
+      alert(res.message);
+    },
+  });
+}
